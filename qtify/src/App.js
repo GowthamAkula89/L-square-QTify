5729fee48128181c2d319a0369b0748ed266ba93
GowthamAkula89 <akulagowthamroyal@gmail.com>
2023-11-06 20:52:29 +0530

    qtify

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import NavBarButton from './components/Nav_Button';
 import './App.css';
-import SearchBar from './components/Searchbar';
 function App() {
   return (
     <div className="App">
-      
-      <NavBarButton/>
-      <SearchBar/>
+      <h1>Hi</h1>
     </div>
   );
 }
