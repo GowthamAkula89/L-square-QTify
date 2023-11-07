310b422ba314f4c30c82c31ae4e6e35a8285ff46
GowthamAkula89 <akulagowthamroyal@gmail.com>
2023-11-07 22:16:58 +0530

    card-qtify-takehome

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
 import HeroSection from './Components/HeroSection';
+import Card from './Components/CardComponet';
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <HeroSection/>
+      <Card/>
     </div>
   );
 }
