aa26e41443113676301387f634b1f1757a90edee
GowthamAkula89 <akulagowthamroyal@gmail.com>
2024-11-06 09:08:00 +0530

    update album type component

--- qtify/src/Components/AlbumsType/index.js
+++ qtify/src/Components/AlbumsType/index.js
@@ -1,30 +1,37 @@
 import { useState } from "react";
-import CardComponent from "../CardComponet";
+import CardComponent from "../CardComponent";
 import "./albumsType.css";
 import Carousel from "../Carousel";
-const AlbumsType=({albums,title,albumsType})=>{
-    const [isCollapsed,setIsCollapsed]=useState(true);
-    return(
+
+const AlbumsType = ({ albums, title, albumsType }) => {
+    const [isCollapsed, setIsCollapsed] = useState(true);
+    return (
         <div className="sub-section">
             <div className="section-heading">
                 <h1 className="section-albums-heading">{title}</h1>
-                <h1 className="section-toggle-button" 
-                    onClick={()=>setIsCollapsed(!isCollapsed)}
+                <h1
+                    className="section-toggle-button"
+                    onClick={() => setIsCollapsed(!isCollapsed)}
                 >
-                    {isCollapsed ? "Show all": "Collapse"}
+                    {isCollapsed ? "Show all" : "Collapse"}
                 </h1>
             </div>
             <div>
-                {isCollapsed ? <Carousel data={albums} title={albumsType}/> : 
-                <div className="section-albums">
-                    {albums.map((album,index) => (
-                        <div key={album.id}>
-                        <CardComponent item={album} title={"album"} index={index}/>
-                        </div>
-                    ))}
-                </div>}
+                {isCollapsed ? (
+                    <Carousel data={albums} title={albumsType} />
+                ) : (
+                    <div className="section-albums">
+                        {albums.map((album, index) => (
+                            <div key={album.id} className="album-title">
+                                <CardComponent item={album} title={"album"} index={index} />
+                                <p>{album.title}</p>
+                            </div>
+                        ))}
+                    </div>
+                )}
             </div>
         </div>
-    )
-}
+    );
+};
+
 export default AlbumsType;
